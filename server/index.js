1327fe8542989ab7dcfe66c0c1f24eae8448d2a5
[Dhara Bakotra] <dalsaniyavasu1234@gmail.com>
2025-06-15 22:54:54 +0530

    Start
    First commite

---
+++ server/index.js
@@ -0,0 +1,15 @@
+// server/index.js
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
